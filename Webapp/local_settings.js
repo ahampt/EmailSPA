eba3793a96cb2e3deb1930816ddfa16dd95a139a
Andy Hampton <ahampton@credera.com>
2013-01-13 16:56:58 -0600

    Remove defunct local settings files

--- Webapp/local_settings.js
+++
@@ -1,6 +0,0 @@
-// URL prefix for static files.
-// Example: "http://media.domain.com/static/"
-var STATIC_URL = 'http://asfdsasdfasdfasdf.com/'
-//
-// Google analytics code to track usage of the site
-// var TRACKING_CODE = ''
